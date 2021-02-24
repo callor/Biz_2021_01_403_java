4f0ac1f1da3198d1fcfeb6950a17427ab8938ba4
callor <callor@callor.com>
2021-02-24 16:49:43 +0900

    Class

--- Java_016_ClassScore/src/com/callor/score/model/ScoreVO.java
+++ Java_016_ClassScore/src/com/callor/score/model/ScoreVO.java
@@ -5,6 +5,17 @@ package com.callor.score.model;
  */
 public class ScoreVO {
 	
+	/*
+	 * 클래스, 인스턴스변수, method 에는
+	 * public 또는 private, protected의 키워드로
+	 * 선언문을 시작할 수 있다.
+	 * 
+	 * 이 3개의 키워드를 scope 한정자라고 한다.
+	 * public : 누구나 읽고 쓰기, 호출이 가능하다
+	 * private : 아무나 읽고 쓰기, 호출이 불가능하다
+	 */
+	private String strNum;
+	
 	public String strName;
 	public int intKor;
 	public int intEng;
@@ -15,5 +26,11 @@ public class ScoreVO {
 	
 	public int intSum;
 	public float floatAvg;
+	
+	
+	
+	
+	
+	
 
 }
