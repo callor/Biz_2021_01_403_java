c1745315ecc1b97c8ff960a62fb85675583b0b0a
callor <callor@callor.com>
2021-03-02 10:51:11 +0900

    interface

---
+++ Java_019_interface/src/com/callor/oop/service/impl/CartServiceImplV2.java
@@ -0,0 +1,42 @@
+package com.callor.oop.service.impl;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.callor.oop.model.CartVO;
+import com.callor.oop.service.CartService;
+
+public class CartServiceImplV2 implements CartService {
+
+	private List<CartVO> cartList;
+	private Scanner scan;
+	
+	/*
+	 * constructor : 생성자 method
+	 * 클래스 이름과 대소문자까지 같은 method
+	 * return type(void, int, String)이 없는 method
+	 * 
+	 * CartServiceImplV2 cs = new CartServiceImplV2();
+	 * 
+	 */
+	public CartServiceImplV2() {
+	
+	}
+	
+	
+	
+	@Override
+	public void inputCart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printCart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
