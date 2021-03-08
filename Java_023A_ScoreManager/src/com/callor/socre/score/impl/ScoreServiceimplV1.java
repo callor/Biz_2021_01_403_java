4b13b68c913eea249e5e8125ef918991a3573225
callor <callor@callor.com>
2021-03-08 13:41:24 +0900

    scoreManager

--- Java_023A_ScoreManager/src/com/callor/socre/score/impl/ScoreServiceimplV1.java
+++ Java_023A_ScoreManager/src/com/callor/socre/score/impl/ScoreServiceimplV1.java
@@ -1,5 +1,8 @@
 package com.callor.socre.score.impl;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -19,11 +22,14 @@ import com.callor.score.values.Values;
  * 		List 객체 선언 및 생성
  * 2. 데이터를 입력 또는 생성하여 List에 담아두기
  */
-public class ScoreServiceimplV1 implements ScoreService{
-	
+public class ScoreServiceimplV1 implements ScoreService {
+
+	private String fileName;
 	private List<ScoreVO> scoreList; // 객체선언
+
 	public ScoreServiceimplV1() {
 		scoreList = new ArrayList<ScoreVO>();
+		fileName = "src/com/callor/score/score.txt";
 	}
 
 	@Override
@@ -32,88 +38,76 @@ public class ScoreServiceimplV1 implements ScoreService{
 	}
 
 	/*
-	 * Random 클래스를 사용하여 점수 생성
-	 * 		makeScore() 에서만 사용할 것이므로
-	 * 		makeScore() method내에서 생성한다.
+	 * Random 클래스를 사용하여 점수 생성 makeScore() 에서만 사용할 것이므로 makeScore() method내에서 생성한다.
 	 */
 	@Override
 	public void makeScore() {
-		
+		// TODO Random 성적표 생성
+
 		// makeScore()는 호출될때마다 20개씩 데이터를
 		// 생성하여 추가한다.
 		// 이전에 저장된 데이터가 있더라도 계속 추가된다
-		
+
 		/*
-		 *  list.remove(위치주소)
-		 *  list에 담긴 특정 위치의 항목을 삭제
-		 *  
-		 *  list.removeAll(다른리스트)
-		 *  list에 담긴 데이터중 다른리스트에 포함된
-		 *  데이터들을 모두 삭제하라
-		 *  
-		 *  트릭을 사용한다
-		 *  scoreList 에 담긴 데이터들 중에서
-		 *  자신의 데이터와 일치하는 값들 모두를
-		 *  삭제하라
-		 *  
+		 * list.remove(위치주소) list에 담긴 특정 위치의 항목을 삭제
+		 * 
+		 * list.removeAll(다른리스트) list에 담긴 데이터중 다른리스트에 포함된 데이터들을 모두 삭제하라
+		 * 
+		 * 트릭을 사용한다 scoreList 에 담긴 데이터들 중에서 자신의 데이터와 일치하는 값들 모두를 삭제하라
+		 * 
 		 */
 		scoreList.removeAll(scoreList);
-		
-		// TODO Auto-generated method stub
+
 		Random rnd = new Random();
-		
-		for(int i = 0 ; i < 20 ; i++) {
-			
+
+		for (int i = 0; i < 20; i++) {
+
 			ScoreVO scoreVO = new ScoreVO();
-			
+
 			/*
-			 * i 값에 따라
-			 * 00001 ~ 00020 까지 문자열을 생성한다
-			 * strNum에 저장한다.
+			 * i 값에 따라 00001 ~ 00020 까지 문자열을 생성한다 strNum에 저장한다.
 			 */
 			String strNum = String.format("2021%03d", i + 1);
-			
+
 			scoreVO.setStNum(strNum);
 			scoreVO.setKor(rnd.nextInt(100) + 1);
-			
+
 			int intEng = rnd.nextInt(100) + 1;
 			scoreVO.setEng(intEng);
-			
+
 			int intMath = rnd.nextInt(100) + 1;
 			scoreVO.setMath(intMath);
-			
+
 			int intMusic = rnd.nextInt(100) + 1;
 			scoreVO.setMusic(intMusic);
-			
+
 			int intHistory = rnd.nextInt(100) + 1;
 			scoreVO.setHistory(intHistory);
 			scoreList.add(scoreVO);
 		} // end for
-		
+
 		// 검사코드
 		// for(ScoreVO vo : scoreList) {
-			// System.out.println(vo.toString());
+		// System.out.println(vo.toString());
 		// }
-		
+
 	}
 
 	@Override
 	public void saveScoreToFile() {
 
-		String fileName = "src/com/callor/score/score.txt";
-		
 		// 객체이름 명명
-		//	클래스이름의 첫글자를 소문자로 하여
-		//	클래스이름을 모두 사용한다.
-		FileWriter fileWriter = null; 	// fw
-		PrintWriter printer = null; 	// pw
-		
+		// 클래스이름의 첫글자를 소문자로 하여
+		// 클래스이름을 모두 사용한다.
+		FileWriter fileWriter = null; // fw
+		PrintWriter printer = null; // pw
+
 		try {
 			fileWriter = new FileWriter(fileName);
 			printer = new PrintWriter(fileWriter);
-			
+
 			// scoreList에 담긴 데이터를 파일로 출력
-			for(ScoreVO vo : scoreList) {
+			for (ScoreVO vo : scoreList) {
 				printer.print(vo.getStNum() + ":");
 				printer.print(vo.getKor() + ":");
 				printer.print(vo.getEng() + ":");
@@ -131,56 +125,94 @@ public class ScoreServiceimplV1 implements ScoreService{
 		}
 	}
 
+	/*
+	 * 성적이 저장되어 있는 score.txt 파일을 읽어서 성적정보를 scoreList에 담기
+	 */
 	@Override
 	public void loadScoreFromFile() {
-		// TODO Auto-generated method stub
 		
+		// 기존에 저장되어 있던 리스트를 모두 제거하자
+		// scoreList = new ArrayList<ScoreVO>()
+		scoreList.removeAll(scoreList);
+		
+		FileReader fileReader = null;
+		BufferedReader buffer = null;
+
+		try {
+			fileReader = new FileReader(fileName);
+			buffer = new BufferedReader(fileReader);
+			while (true) {
+				String reader = buffer.readLine();
+				if (reader == null) {
+					break;
+				}
+				// 읽어들인 문자열을 콜론(:)을 기준으로
+				// 잘라서 배열로 만들어 달라
+				// scores 배열에 담아라
+				String scores[] = reader.split(":");
+
+				// 필드생성자를 호출하여
+				// 인스턴스 변수에 저장할 값을 전달하면서
+				// 동시에 객체를 초기화(생성)
+				ScoreVO vo = new ScoreVO(
+						scores[0], // stNum
+						Integer.valueOf(scores[1]), // kor
+						Integer.valueOf(scores[2]), // eng
+						Integer.valueOf(scores[3]), // math
+						Integer.valueOf(scores[4]), // music
+						Integer.valueOf(scores[5]) // history
+				);
+				scoreList.add(vo);
+			}
+			buffer.close();
+			fileReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(fileName + " 파일이 없습니다");
+		} catch (IOException e) {
+			System.out.println("파일을 읽는동안 문제 발생");
+		}
+		this.printScore();
+
 	}
 
 	private void totalAndAvg() {
-		for(ScoreVO vo : scoreList) {
+		for (ScoreVO vo : scoreList) {
 			int sum = vo.getKor();
 			sum += vo.getEng();
 			sum += vo.getMath();
 			sum += vo.getMusic();
 			sum += vo.getHistory();
-			
-			float avg = (float)sum / 5;
-			
+
+			float avg = (float) sum / 5;
+
 			vo.setTotal(sum);
 			vo.setAvg(avg);
 		}
 	}
-	
+
 	@Override
 	public void printScore() {
 
 		// 출력전에 총점 평균 계산
 		this.totalAndAvg();
-		
+
 		System.out.println(Values.dLine);
 		System.out.println("빛나라 고교 성적처리 시스템 2021");
 		System.out.println(Values.dLine);
-		System.out.println("학번\t국어\t영어\t수학\t"
-				+ "음악\t국사\t총점\t평균\t석차");
+		System.out.println("학번\t국어\t영어\t수학\t" + "음악\t국사\t총점\t평균\t석차");
 		System.out.println(Values.sLine);
-		for(ScoreVO vo : scoreList) {
+		for (ScoreVO vo : scoreList) {
 			System.out.print(vo.getStNum() + "\t");
 			System.out.print(vo.getKor() + "\t");
 			System.out.print(vo.getEng() + "\t");
 			System.out.print(vo.getMath() + "\t");
 			System.out.print(vo.getMusic() + "\t");
 			System.out.print(vo.getHistory() + "\t");
-			
 			System.out.print(vo.getTotal() + "\t");
-			
-			System.out.printf("%3.2f\t",vo.getAvg());
-
+			System.out.printf("%3.2f\t", vo.getAvg());
 			System.out.print(vo.getRank() + "\n");
 		}
 		System.out.println(Values.dLine);
 	}
 
 }
-
-
